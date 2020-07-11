2d921c7d9f82e9393265f50f278f56e3149de83d
“Vimalate <1902644369@qq.com>
2020-07-11 21:47:02 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
+<!--
+ * @Author: Vimalakirti
+ * @Date: 2020-07-11 21:33:55
+ * @LastEditTime: 2020-07-11 21:36:40
+ * @Description: 
+ * @FilePath: \vue-cars\car\src\App.vue
+--> 
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -17,16 +20,5 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/views/index/index.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: Vimalakirti
+ * @Date: 2020-07-11 21:37:05
+ * @LastEditTime: 2020-07-11 21:38:06
+ * @Description: 
+ * @FilePath: \vue-cars\car\src\views\index\index.vue
+--> 
+<template>
+  <div>Index</div>
+</template>
+
+<script>
+export default {
+  name:'Index',
+  data () {
+    return {
+    };
+  },
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
